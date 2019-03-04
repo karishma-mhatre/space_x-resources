fcd2dcfa6300bfc5b2338ecab19a137beb6928a4
Karishma Mhatre <karishmamhatre077@gmail.com>
2019-03-04 16:34:00 +0400

    Updates

---
+++ precache-manifest.ceb239ed078324c78795dccaed1afc2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85466f803cd157e1e9cb5de796375074",
+    "url": "/space_x-resources/static/media/spacex-launchpad.85466f80.jpg"
+  },
+  {
+    "revision": "b659704d2556a1c07273",
+    "url": "/space_x-resources/static/js/runtime~main.b659704d.js"
+  },
+  {
+    "revision": "a95798974d91b4002676",
+    "url": "/space_x-resources/static/js/main.a9579897.chunk.js"
+  },
+  {
+    "revision": "9ce99801997570c253b6",
+    "url": "/space_x-resources/static/js/2.9ce99801.chunk.js"
+  },
+  {
+    "revision": "a95798974d91b4002676",
+    "url": "/space_x-resources/static/css/main.362ba864.chunk.css"
+  },
+  {
+    "revision": "17df9a815cd79edb954489d56c8b9e0a",
+    "url": "/space_x-resources/index.html"
+  }
+];
